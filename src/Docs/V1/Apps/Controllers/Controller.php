d72ff23400ae03046362a811ed271de065073b2e
Steve <34465153+xxl4@users.noreply.github.com>
2025-01-01 13:12:22 +0800

    update version

--- src/Docs/V1/Apps/Controllers/Controller.php
+++ src/Docs/V1/Apps/Controllers/Controller.php
@@ -3,7 +3,7 @@ namespace NexaMerchant\Apps\Docs\V1\Apps\Controllers;
 
 /**
  * @OA\Info(
- *      version="1.0.5",
+ *      version="1.0.6",
  *      title="NexaMerchant Apps Rest API Documentation",
  *      description="NexaMerchant Apps Rest API Documentation",
  *
