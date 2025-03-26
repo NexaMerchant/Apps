0a19f289320b283c5cc2f570443ca3238b877b36
Steve <34465153+xxl4@users.noreply.github.com>
2025-03-26 09:40:32 +0800

    fix create platform

--- src/Console/Commands/CreatePlatform.php
+++ src/Console/Commands/CreatePlatform.php
@@ -13,11 +13,24 @@ class CreatePlatform extends CommandInterface{
     public function handle(){
         $this->info('Create a new nexa platform use docker, kubernetes and souce code');
 
-        
+
 
     }
 
     public function checkEnv(){
         $this->info('Check environment');
+
+        // check docker
+        $this->info('Check docker');
+
+        // check kubernetes
+        $this->info('Check kubernetes');
+
+        // check source code
+        $this->info('Check source code');
+
+        // check ssh
+
+        // check git
     }
 }
